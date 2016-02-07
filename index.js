c1fa549f5b97782cf44314ee3f09addd290e91eb
Eric <spamquagmire@gmail.com>
2016-02-07 03:28:04 -0500

    Updated menu

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var prompt = require('prompt');
 var colors = require('colors');
+// var Nightmare = require('nightmare');
+var http = require('http');
+// var Promise = require('q');
+// var fs = require('fs');
+// var cheerio = require('cheerio');
+var Promise = require('q');
 
 var examples = require('./examples');
 
@@ -8,7 +14,8 @@ var examples = require('./examples');
 console.log('\n\nSelect which test you would like to run:\n'.underline.white.bold);
 console.log('-- Test 1: Using request & cheerio');
 console.log('-- Test 2: Simple scraping with request & cheerio');
-console.log('-- Test 3: Using Nightmare & cheerio\n');
+console.log('-- Test 3: Using Nightmare & cheerio, scrape content off of an Angular site');
+console.log('-- Test 4: Take screenshots as you programmatically interact with website\n');
 
 
 prompt.start();
@@ -27,9 +34,13 @@ if (err) { return onErr(err); }
         case '3':
             examples.third();
             break;
+        case '4':
+            examples.fourth();
+            break;
         default:
-            console.log('Errrrr');
+            console.log('\nExiting\n');
     }
+
 });
 
 function onErr(err) {
